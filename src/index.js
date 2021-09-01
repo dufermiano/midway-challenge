4573be69fcbc6acd55abb330c3addc879ca57b4e
Eduardo Fermiano Rocha <Eduardo.Rocha@sky.com.br>
2021-09-01 00:29:22 -0300

    Primeiro setup projeto + Docker

---
+++ src/index.js
@@ -0,0 +1,26 @@
+const express = require("express");
+const cors = require("cors");
+
+const app = express();
+
+app.use(cors());
+app.disable("x-powered-by");
+
+const port = process.env.PORT || 3000;
+
+const host = "0.0.0.0";
+
+app.get("/", (req, res) => {
+  res.status(200).json({ error: false, data: { message: "Teste" } });
+});
+
+app.use(express.json());
+app.use(
+  express.urlencoded({
+    extended: true,
+  })
+);
+
+app.listen(port, host);
+
+console.log("Servidor escutando a porta", port);
