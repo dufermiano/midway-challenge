c52810894a2adfbdf43f1de697994de2e9d66e4f
Eduardo Fermiano Rocha <Eduardo.Rocha@sky.com.br>
2021-09-02 01:46:29 -0300

    reafactor

--- scripts/01.sql
+++ scripts/01.sql
@@ -1,14 +1,14 @@
-CREATE TABLE IF NOT EXISTS PRODUCTS(
+CREATE TABLE IF NOT EXISTS Products(
     id INT(6) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
     nome VARCHAR(255) NOT NULL,
     valor DECIMAL(5,2) NOT NULL,
     estoque INT(50),
-    tamanho CHAR(3),
+    tamanho VARCHAR(3),
     tipo VARCHAR(30),
     descricao VARCHAR(255),
-    dataCadastro VARCHAR(255),
-    dataAtualizacao VARCHAR(255)
+    dataCadastro DATETIME,
+    dataAtualizacao DATETIME
 ) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci;
 
-INSERT INTO PRODUCTS(nome, valor, estoque, tamanho, tipo, descricao, dataCadastro) VALUES ("Calça", "100.00", 5, "M", "Jeans", "Calça Jeans", now());
-INSERT INTO PRODUCTS(nome, valor, estoque, tamanho, tipo, descricao, dataCadastro) VALUES ("Calça", "100.00", 3, "m", "Jeans", "Calça Jeans m", now());
+INSERT INTO Products(nome, valor, estoque, tamanho, tipo, descricao, dataCadastro) VALUES ("Calça", "100.00", 5, "M", "Jeans", "Calça Jeans", now());
+INSERT INTO Products(nome, valor, estoque, tamanho, tipo, descricao, dataCadastro) VALUES ("Calça", "100.00", 3, "m", "Jeans", "Calça Jeans m", now());
