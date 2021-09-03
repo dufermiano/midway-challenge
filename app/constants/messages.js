5c0409e114c6aed44e9ac081299e7742b88812cd
Eduardo Fermiano Rocha <Eduardo.Rocha@sky.com.br>
2021-09-02 21:30:35 -0300

    creating messages object

---
+++ app/constants/messages.js
@@ -0,0 +1,11 @@
+const messages = {
+  productCreated: 'Produto criado com sucesso.',
+  productUpdated: 'Produto alterado com sucesso.',
+};
+
+const errorMessages = {};
+
+module.exports = {
+  messages,
+  errorMessages,
+};
